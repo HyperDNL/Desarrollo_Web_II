ed8812f83c15367722d25269c41ed7ae39f7d3b2
Daniel López <hyperdaniel579exe@gmail.com>
2023-03-07 13:35:48 -0600

    creación de interfaces para el proyecto

---
+++ practice-use-context/src/interfaces/UsersType.ts
@@ -0,0 +1,13 @@
+export interface IUser {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+  company: {
+    name: string;
+  };
+}
+export type UserContextType = {
+  users?: IUser[];
+  getUsers: Function;
+};
