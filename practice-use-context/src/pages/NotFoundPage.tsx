2f75d1873fa3ff670694b499666407d7e6e97c11
Daniel López <hyperdaniel579exe@gmail.com>
2023-03-08 14:26:52 -0600

    page not found implemented

--- practice-use-context/src/pages/NotFoundPage.tsx
+++ practice-use-context/src/pages/NotFoundPage.tsx
@@ -1,5 +1,38 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 export const NotFoundPage = () => {
-  return <div>Not Found Page</div>;
+  return (
+    <>
+      <NotFoundContainer>
+        <h1>
+          <span className="red-text">Oops!</span> Page Not Found
+        </h1>
+        <p>The page you were looking for does not exist.</p>
+        <Link to={"/"} className="link-home">
+          Go Home
+        </Link>
+      </NotFoundContainer>
+    </>
+  );
 };
+
+const NotFoundContainer = styled.div`
+  width: 100%;
+  height: 80vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  .red-text {
+    color: #ed343e;
+  }
+
+  .link-home {
+    text-decoration: none;
+    color: #f0f0f0;
+    font-weight: bold;
+  }
+`;
