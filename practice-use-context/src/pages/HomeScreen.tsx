b064260ced0f9fae3aa4d5c41175b393e3a19336
Daniel López <hyperdaniel579exe@gmail.com>
2023-03-08 14:28:09 -0600

    Update Home Screen

--- practice-use-context/src/pages/HomeScreen.tsx
+++ practice-use-context/src/pages/HomeScreen.tsx
@@ -1,11 +1,14 @@
 import React from "react";
+import styled from "styled-components";
 import { useUsers } from "../context/usersContext";
+import { IUser } from "../interfaces/UsersType";
+import { UserCard } from "../components/UserCard";
 
 export const HomeScreen = () => {
-  const users = useUsers()?.users;
+  const { users }: any = useUsers();
 
-  const destructuringUsers = users?.map(
-    ({ id, name, email, phone, company }) => {
+  const mappingUsers: IUser[] = users?.map(
+    ({ id, name, email, phone, company }: IUser) => {
       return {
         id: id,
         name: name,
@@ -18,5 +21,29 @@ export const HomeScreen = () => {
     }
   );
 
-  return <></>;
+  return (
+    <>
+      <HomeContainer>
+        {mappingUsers?.map(({ id, name, email, phone, company }: IUser) => (
+          <UserCard
+            key={id}
+            id={id}
+            name={name}
+            email={email}
+            phone={phone}
+            company={company}
+          />
+        ))}
+      </HomeContainer>
+    </>
+  );
 };
+
+const HomeContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  column-gap: 2rem;
+  row-gap: 2rem;
+  justify-items: stretch;
+  align-items: stretch;
+`;
