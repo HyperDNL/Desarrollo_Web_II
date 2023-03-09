e29932584e1c6584dfbcbc3e217efe32ef33a078
Daniel López <hyperdaniel579exe@gmail.com>
2023-03-09 01:37:44 -0600

    Objeto de dispositivos responsivos actualizado

--- practice-use-context/src/styles/responsive.ts
+++ practice-use-context/src/styles/responsive.ts
@@ -10,11 +10,11 @@ const size = {
 
 export const device = {
   mobileS: `(min-width: ${size.mobileS})`,
+  mobileSS: `(max-width: ${size.mobileS})`,
   mobileM: `(min-width: ${size.mobileM})`,
   mobileL: `(min-width: ${size.mobileL})`,
   tablet: `(min-width: ${size.tablet})`,
   laptop: `(min-width: ${size.laptop})`,
   laptopL: `(min-width: ${size.laptopL})`,
   desktop: `(min-width: ${size.desktop})`,
-  desktopL: `(min-width: ${size.desktop})`,
 };
