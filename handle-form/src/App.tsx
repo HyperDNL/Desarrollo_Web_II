c5862d21826965e5000945bdf6567460936be007
Daniel López <hyperdaniel579exe@gmail.com>
2023-04-28 17:26:38 -0600

    updated page router

--- handle-form/src/App.tsx
+++ handle-form/src/App.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { HomePage, UserFormPage, NotFoundPage } from "./pages/Pages";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/user-form" element={<UserFormPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
