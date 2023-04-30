8b5aa037bdaf11d7e8a89e218128864841edbff1
Daniel López <hyperdaniel579exe@gmail.com>
2023-04-30 07:48:54 -0600

    page not found implemented

--- handle-form/src/pages/NotFoundPage.tsx
+++ handle-form/src/pages/NotFoundPage.tsx
@@ -1,5 +1,42 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { webLightTheme } from "@fluentui/react-components";
 
 export const NotFoundPage = () => {
-  return <div>Not Found Page</div>;
+  return (
+    <>
+      <NotFoundContainer>
+        <h1>
+          <span className="red-text">Oops!</span> Page Not Found
+        </h1>
+        <p>The page you were looking for does not exist.</p>
+        <Link to={"/"} className="link-home">
+          Go Home
+        </Link>
+      </NotFoundContainer>
+    </>
+  );
 };
+
+const NotFoundContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  .red-text {
+    color: ${webLightTheme.colorPaletteBlueBorderActive};
+  }
+  .link-home {
+    text-decoration: none;
+    color: #f0f0f0;
+    background-color: ${webLightTheme.colorPaletteBlueBorderActive};
+    font-weight: bold;
+    padding: 1rem;
+    border-radius: 1rem;
+    &:hover {
+      background-color: ${webLightTheme.colorPaletteRoyalBlueBorderActive};
+    }
+  }
+`;
