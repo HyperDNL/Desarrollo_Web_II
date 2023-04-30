2d56a07bb57c8a32d117e2cbd7a30736c2ec093b
Daniel López <hyperdaniel579exe@gmail.com>
2023-04-30 07:42:58 -0600

    updated app routes, settings, and styles

--- handle-form/src/App.tsx
+++ handle-form/src/App.tsx
@@ -1,16 +1,27 @@
 import React from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import { HomePage, UserFormPage, NotFoundPage } from "./pages/Pages";
+import { FluentProvider, webLightTheme } from "@fluentui/react-components";
+import { HomePage, UserFormPage, NotFoundPage, UsersPage } from "./pages/Pages";
+import { Container } from "@mui/material";
+import { Toaster } from "react-hot-toast";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/user-form" element={<UserFormPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </BrowserRouter>
+    <FluentProvider theme={webLightTheme}>
+      <BrowserRouter>
+        <Navbar />
+        <Container maxWidth="xl" style={{ marginTop: 20 }}>
+          <Toaster />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/users" element={<UsersPage />} />
+            <Route path="/user-form/:id" element={<UserFormPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </Container>
+      </BrowserRouter>
+    </FluentProvider>
   );
 }
 
