96a5a5d113972a429328759b454187d7d2456132
Daniel López <hyperdaniel579exe@gmail.com>
2023-04-30 07:49:10 -0600

    users page implemented

---
+++ handle-form/src/pages/UsersPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TableActions from "../components/TableActions";
+import { DividerSection } from "../components/DividerSection";
+
+export const UsersPage = () => {
+  return (
+    <>
+      <DividerSection text="Users" />
+      <TableActions />
+    </>
+  );
+};
